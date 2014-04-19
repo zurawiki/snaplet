cdf98b3435d4ecd48ccaac2989c5bd852dd7889e
rzurawicki <rzurawicki@gmail.com>
2014-04-18 21:55:26 -0400

    Initial Commit

---
+++ SnapChatRoulette/RZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RZAppDelegate.h
+//  SnapChatRoulette
+//
+//  Created by Roger Zurawicki on 4/18/14.
+//  Copyright (c) 2014 Roger Zurawicki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SnapChatRoulette/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SnapChatRoulette
+//
+//  Created by Roger Zurawicki on 4/18/14.
+//  Copyright (c) 2014 Roger Zurawicki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RZAppDelegate class]));
+    }
+}
